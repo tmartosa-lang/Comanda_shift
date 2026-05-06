d96d00527ae7ed0659b69854a35fd643ceb90fba
tony <tmartosa@esliceu.net>
2026-05-06 16:08:50 +0200

    suma de parells i senars entre 1 i n

---
+++ 07_suma_parells copy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]
+then
+    echo "Error: se requiere un único paràmetro"
+    exit 1
+fi
+
+#comprobar que realmente tenemos un número
+if ! [[ $1 =~ ^[0-9]+$ ]]; then
+    echo "Error: se requiere un número"
+    exit 1
+fi
+
+echo "vamos a calcular el sumatorio de $1"
+
+#recoger valor de n
+n=$1
+suma=0
+
+#bucle for
+for (( i=2;i<=n;i+=2 ))
+do
+    echo "i: $i valor de n $n"
+    suma=$((suma+i))
+done
+
+echo "la suma de los números pares entre 1 y $n vale $suma"
+#calcular el sumatorio de un número, por ejemplo el sumatorio de 5 sería 1+2+3+4+5=15
+exit 0
---
+++ 09_ejemplo_for.sh
@@ -0,0 +1,8 @@
+#bucle for
+
+n=10
+
+for (( i=1;i<n;i++ ))
+do
+    echo "i: $i n: $n"
+done
